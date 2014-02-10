99a60fedf176a666016f8145d53144f3bd0ef4dd
sean lyons <sean@hexault.com>
2014-02-10 16:45:24 -0600

    progress

--- apps/breed3/breed.js
+++ apps/breed3/breed.js
@@ -1,6 +1,9 @@
 var saved_bg;
 var saved_ele;
+var index_bg = '';
 var index_quants = new Array();
+var color_rand = 16777215;
+var win_similarity = 135;
 
 generateIndex();
 generateGrid();
@@ -8,7 +11,11 @@ init();
 
 function init() {
 	var x = document.getElementsByClassName('square');
+	var index = document.getElementById('index_square');
 	for (var i=0;i<x.length;i++) {
+		if (x == index) {
+			continue;
+		}		
 		x[i].onclick = function () {
 			var bg = this.style.backgroundColor;
 			var ele = this;
@@ -35,19 +42,22 @@ function init() {
 				
 				color = getRandomColor();
 				saved_ele.innerHTML = color;
+				toggleSelectedBorder(saved_ele, 'off');
 				saved_ele.style.backgroundColor = color;
 				this.innerHTML = color_string;
 				this.style.backgroundColor = color_string;
+				
                
                 //Write to history what happened.
                 writeChildHistory(saved_bg, bg, color_string, color); //color _1_ was dragged onto color _2_, combined to form _3_, _4_ replaced the consumed one.
                 
                 //Check to see if child is within winning distance of index.
-
+				checkvictory_adjacency(color_string);
+					
 				saved_bg = null;
 				saved_ele = null;
-                
 			} else {
+				toggleSelectedBorder(ele, 'on');
 				saved_ele = ele;
 				saved_bg = bg;
 			}
@@ -55,7 +65,10 @@ function init() {
 	}
 }
 
-function getRandomColor(solid_chance = 15) {
+function getRandomColor(solid_chance) {
+	if (isNaN(solid_chance)) {
+		solid_chance = 15;
+	}
 	var color = '#';
     rand = random(solid_chance);
     switch (rand) {
@@ -71,7 +84,7 @@ function getRandomColor(solid_chance = 15) {
             return '#ff0000';
         default:
             while (color.length != 7) {
-                color = '#' + random(16777215).toString(16);
+                color = '#' + random(color_rand).toString(16);
             }
             return color;
     }
@@ -79,7 +92,8 @@ function getRandomColor(solid_chance = 15) {
 
 function generateIndex() {
 	var index = document.getElementById('index_square');
-	index.style.backgroundColor = getRandomColor(40);
+	index_bg = getRandomColor(40);
+	index.style.backgroundColor = index_bg;
 	index.setAttribute("class", "bar");
 }
 
@@ -87,41 +101,97 @@ function generateGrid() {
 console.log('init generateGrid');
 	for (h = 0; h < 5; h++) {
 		for (w = 0; w < 4; w++) {
-			generateNewSquare(w, h);
+			// if (w == 1) {
+				// var new_br = document.createElement("br");
+				// document.body.insertBefore(new_br, last_square);
+			// }
+			var last_square = generateNewSquare(w, h);
 		}
 	}
 }
 
 function generateNewSquare(w, h) {
-	var new_div= document.createElement("div");
+	var new_div = document.createElement("div");
 	terminator_node = document.getElementById('terminator_node');
 	document.body.insertBefore(new_div, terminator_node);
 	new_div.setAttribute("class", "square");
 	var color = getRandomColor();
 	new_div.innerHTML = color;
 	new_div.style.backgroundColor = color;
-
+	return new_div;
 }
 
-function random(max = 1) { 
+function random(max) {
+	if (isNaN(max)) {
+		max = 1;
+	}
     return Math.floor(max * Math.random());
 }
 
 //color _1_ was dragged onto color _2_, combined to form _3_, _4_ replaced the consumed one.
 function writeChildHistory(saved_bg, bg, color_string, color) {
+	saved_bg = rgbdecHex(saved_bg);
+	bg = rgbdecHex(bg);
     document.getElementById('child_history').innerHTML = 'You combined '+saved_bg+' with '+bg+' to form '+color_string+'!<br/>';
-    console.log('You combined '+saved_bg+' with '+bg+' to form '+color_string+'!<br/>');
     document.getElementById('new_history').innerHTML = color+' took '+saved_bg+"'s place.<br/>";
 }
 
-function rgbToHex(R,G,B) {
-    return toHex(R) + toHex(G) + toHex(B);
+function rgbdecHex(rgb_str) {
+	var color = '#';
+	
+	rgb_str = rgb_str.replace('rgb(', '');
+	rgb_str = rgb_str.replace(')', '');
+	var rgb_arr = rgb_str.split(", ");
+
+	for (i = 0; i < 3; i++) {
+		color += decHex(rgb_arr[i]);
+	}
+	return color;
 }
-function toHex(n) {
+
+function decHex(n) {
     n = parseInt(n,10);
     if (isNaN(n)) {
         return "00";
     }
     n = Math.max(0,Math.min(n,255));
-    return "0123456789ABCDEF".charAt((n - n % 16) / 16) + "0123456789ABCDEF".charAt(n % 16);
+    return "0123456789abcdef".charAt((n - n % 16) / 16) + "0123456789abcdef".charAt(n % 16);
+}
+
+function hexDec(n) {
+	return parseInt(n, 16);
+}
+
+function checkvictory_adjacency(hex) {
+	var rgbs = ['Red', 'Green', 'Blue'];
+	var victory_adjacency = '';
+	var sum_off = 0;
+	for (i = 0; i < 3; i++) {
+		var hex_diff = hexDec(hex.substring(1 + (2*i), 3 + (2*i)));
+		var index_diff = hexDec(index_bg.substring(1 + (2*i), 3 + (2*i)));
+		
+		var off = (hex_diff - index_diff).toFixed(0);
+		victory_adjacency += '<span style="color:'+rgbs[i]+'">'+rgbs[i] + ' is off by ' + off + '.</span>&nbsp;';
+		sum_off += Math.abs(parseInt(off));
+	}
+	sum_off -= win_similarity;
+	
+	victory_adjacency += ' (' + sum_off + ' away from victory)';
+	if (sum_off <= 0) {
+		victory_adjacency += 'AND YOU WIN!';
+	}
+	
+	console.log('victory_adjacency = ' + victory_adjacency);
+	document.getElementById('victory_adjacency').innerHTML = victory_adjacency;
+}
+
+function toggleSelectedBorder(element, on_or_off) {
+	switch (on_or_off) {
+		case 'on':
+			element.style.border = '5px solid orange';
+			break;
+		case 'off':
+			element.style.border = '5px solid #ccc';
+			break;
+	}
 }
--- apps/breed3/index.php
+++ apps/breed3/index.php
@@ -9,30 +9,23 @@
 				border:5px solid #ccc;
 				margin:5px;
 			}
-			#adjacency {
-				background:pink;
-				height:200px;
-				width:200px;
-				font-size:400px;
-			}
 		</style>		
         <!--link rel="stylesheet" type="text/css" href="breed.css"-->
     </head>
     <body>
-        <div id='hud'>
-			<div id='index_square' class='index_square'></div>
-		</div>
-		<br/>
-        <div id='selection_space'></div>
-		<div id='terminator_node'></div>
-		<div id='child_history'>
-            You have not made any moves yet.
-        </div>
-		<div id='new_history'>
-        </div>
-		<div id='adjacency'>
-			adjacency!
-        </div>
+        <!--div id='container'-->
+            <div id='hud'>
+                <div id='index_square' class='index_square'></div>
+            </div>
+            <br/>
+            <div id='selection_space'></div>
+            <div id='terminator_node'></div>
+            <div id='child_history'>
+                You have not made any moves yet.
+            </div>
+            <div id='new_history'></div>
+            <div id='victory_adjacency'></div>
+        <!--/div-->
     </body>
 	<script src="breed.js"></script>
 </html>
