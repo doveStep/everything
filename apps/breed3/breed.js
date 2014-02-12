5d8605146f54f6c2e62f87da98aa187e41b648cf
sean lyons <sean@hexault.com>
2014-02-12 05:04:02 -0600

    Added turn counting Added jquery, so i can convert all this vanilla js to it. Added the cat pngs, which look boss. More logging Removed the precise color combination info

--- apps/breed3/breed.js
+++ apps/breed3/breed.js
@@ -4,6 +4,7 @@ var index_bg = '';
 var index_quants = new Array();
 var color_rand = 16777215;
 var win_similarity = 135;
+var turns = 0;
 
 generateIndex();
 generateGrid();
@@ -47,7 +48,9 @@ function init() {
 				this.innerHTML = color_string;
 				this.style.backgroundColor = color_string;
 				
-               
+				$( ".square" ).wrapInner( "<img class='cat_png' src='cat.png' />");
+				turns++;
+			   
                 //Write to history what happened.
                 writeChildHistory(saved_bg, bg, color_string, color); //color _1_ was dragged onto color _2_, combined to form _3_, _4_ replaced the consumed one.
                 
@@ -105,6 +108,7 @@ console.log('color = ' + color);
 function generateIndex() {
 	var index = document.getElementById('index_square');
 	index_bg = getRandomColor(40, 'index');
+console.log('index_bg = '+index_bg);
 	index.style.backgroundColor = index_bg;
 	index.setAttribute("class", "bar");
 }
@@ -120,16 +124,23 @@ console.log('init generateGrid');
 			var last_square = generateNewSquare(w, h);
 		}
 	}
+	$( ".square" ).wrapInner( "<img class='cat_png' src='cat.png' />");
 }
 
 function generateNewSquare(w, h) {
 	var new_div = document.createElement("div");
+	var new_cat = document.createElement("img");
+	
 	terminator_node = document.getElementById('terminator_node');
 	document.body.insertBefore(new_div, terminator_node);
+	
 	new_div.setAttribute("class", "square");
 	var color = getRandomColor(15, 'grid');
 	new_div.innerHTML = color;
 	new_div.style.backgroundColor = color;
+
+	new_cat.setAttribute("src", "cat.png");
+	
 	return new_div;
 }
 
@@ -146,8 +157,8 @@ console.log('random = ' + random);
 function writeChildHistory(saved_bg, bg, color_string, color) {
 	saved_bg = rgbdecHex(saved_bg);
 	bg = rgbdecHex(bg);
-    document.getElementById('child_history').innerHTML = 'You combined '+saved_bg+' with '+bg+' to form '+color_string+'!<br/>';
-    document.getElementById('new_history').innerHTML = color+' took '+saved_bg+"'s place.<br/>";
+    //document.getElementById('child_history').innerHTML = 'You combined '+saved_bg+' with '+bg+' to form '+color_string+'!<br/>';
+    //document.getElementById('new_history').innerHTML = color+' took '+saved_bg+"'s place.<br/>";
 }
 
 function rgbdecHex(rgb_str) {
@@ -186,6 +197,7 @@ function checkvictory_adjacency(hex) {
 		
 		var off = (hex_diff - index_diff).toFixed(0);
 		if (off == -255) {
+console.log('off was -255, is now 0');
 			off = 0;
 		}
 		victory_adjacency += '<span style="color:'+rgbs[i]+'">'+rgbs[i] + ' is off by ' + off + '.</span>&nbsp;';
@@ -195,7 +207,7 @@ function checkvictory_adjacency(hex) {
 	
 	victory_adjacency += ' (' + sum_off + ' away from victory)';
 	if (sum_off <= 0) {
-		victory_adjacency += 'AND YOU WIN!';
+		victory_adjacency += 'AND YOU WIN! ( ' + turns + ' turns)';
 	}
 	
 	console.log('victory_adjacency = ' + victory_adjacency);
@@ -208,7 +220,7 @@ function toggleSelectedBorder(element, on_or_off) {
 			element.style.border = '5px solid orange';
 			break;
 		case 'off':
-			element.style.border = '5px solid #ccc';
+			element.style.border = '5px solid #CCC';
 			break;
 	}
 }
--- apps/breed3/index.php
+++ apps/breed3/index.php
@@ -1,15 +1,22 @@
 <html>
     <head>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
 		<style>
+			.cat_png {
+				height:100%;
+				width:100%;
+			}
 			.square {
 				position:relative;
 				height:100px;
 				width:100px;
 				float:left;
-				border:5px solid #ccc;
+				border:5px solid #CCC;
 				margin:5px;
 			}
-		</style>		
+		</style>
         <!--link rel="stylesheet" type="text/css" href="breed.css"-->
     </head>
     <body>
@@ -20,11 +27,9 @@
             <br/>
             <div id='selection_space'></div>
             <div id='terminator_node'></div>
-            <div id='child_history'>
-                You have not made any moves yet.
-            </div>
+            <div id='child_history'></div>
             <div id='new_history'></div>
-            <div id='victory_adjacency'></div>
+            <div id='victory_adjacency'>Breed two cats together by clicking one, and then clicking another!</div>
         <!--/div-->
     </body>
 	<script src="breed.js"></script>
